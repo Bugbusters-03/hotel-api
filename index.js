49e0a0d1cc42efd4d954b89d47ed05ccbe2675f6
ibr <ibr.seckin@gmail.com>
2024-07-31 13:56:01 +0300

    13 comment

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const express  = require('express');
 const app  = express();
 
 
-app.use(express.json())
+// app.use(express.json())
 
 /* -------------------------- express-async-errors -------------------------- */
 require('express-async-errors')
