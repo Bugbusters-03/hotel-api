ba4f854f13848646bc917a5e03322bf46ae8cdd5
ibr <ibr.seckin@gmail.com>
2024-07-31 14:04:53 +0300

    own brnach up

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const app  = express();
 
 
 // app.use(express.json())
+app.use(express.json())
+// app.use(express.json())
 
 /* -------------------------- express-async-errors -------------------------- */
 require('express-async-errors')
